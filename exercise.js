7f68d155b297899d9dde00251df5c72f5946d80c
FranzRivas-Ksquare <franz.rivas@itksquare.edu.mx>
2022-09-29 19:26:06 -0500

    finish activity

--- exercise.js
+++ exercise.js
@@ -1,14 +1,19 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const bubbleSort = (arr) => {
     /* Only make changes below this comment */
-    
+    for(let i = 0; i < arr.length; i++){
+        for(let j = 0; j < ( arr.length - i -1 ); j++){
+            if(arr[j] > arr[j+1]){
+            
+            let temp = arr[j]
+            arr[j] = arr[j + 1]
+            arr[j+1] = temp
+            }
+        }
+    }
     return arr
     /* Only make changes below this comment */
 }
-
-
-
-
 
 /** DO NOT CHANGE THE LINE BELOW **/
 module.exports.bubbleSort = bubbleSort;
